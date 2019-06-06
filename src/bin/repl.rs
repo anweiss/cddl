4f9bd23977ff5048406b511ee1be513327fab63f
Andrew Weiss <anweiss@github.com>
2019-06-06 17:32:47 -0400

    repl adjustments

--- src/bin/repl.rs
+++ src/bin/repl.rs
@@ -1,13 +1,8 @@
 use cddl::repl;
 use std::{error::Error, io};
-use whoami;
 
 fn main() -> Result<(), Box<Error>> {
-  let username = whoami::username();
-
-  println!("Hello {}! This is the CDDL language!", username);
-
-  println!("Feel free to type in commands");
+  println!("CDDL REPL");
 
   let input = io::stdin();
   let output = io::stdout();
--- src/repl.rs
+++ src/repl.rs
@@ -3,6 +3,7 @@ use std::{
   error,
   io::{BufRead, Write},
 };
+use termion::{clear, cursor};
 
 const PROMPT: &[u8] = b">> ";
 
@@ -14,12 +15,26 @@ pub fn start<R: BufRead, W: Write>(mut reader: R, mut writer: W) -> Result<(), B
     let mut line = String::new();
     reader.read_line(&mut line)?;
 
-    let mut l = Lexer::new(&line);
-    let mut p = Parser::new(&mut l)?;
+    if let Some(s) = control(&line) {
+      writer.write_all(s.as_bytes())?;
+    } else {
+      let mut l = Lexer::new(&line);
+      let mut p = Parser::new(&mut l)?;
 
-    let cddl = p.parse_cddl()?;
+      let cddl = p.parse_cddl()?;
 
-    writer.write_all(format!("{:?}\n", cddl).as_bytes())?;
+      writer.write_all(format!("{:?}\n", cddl).as_bytes())?;
+    }
+
     writer.flush()?;
   }
 }
+
+fn control(line: &str) -> Option<String> {
+  match line {
+    "clear()\n" => Some(format!("{}{}", clear::All, cursor::Goto(1, 1))),
+    "quit()\n" | "exit()\n" => std::process::exit(0),
+    _ => None,
+  }
+}
+
