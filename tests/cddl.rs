6a32cfa797215e349f019bf4971da9a1c8f04bf5
Andrew Weiss <anweiss@github.com>
2019-06-06 17:34:43 -0400

    additional cddl document tests

--- tests/cddl.rs
+++ tests/cddl.rs
@@ -5,8 +5,11 @@ use std::fs;
 
 #[test]
 fn verify_cddl_compiles() {
-  let cddl_contents = fs::read_to_string("tests/cddl/reputon.cddl").unwrap();
-  let mut l = Lexer::new(&cddl_contents);
-  let mut p = Parser::new(&mut l).unwrap();
-  println!("{:#?}", p.parse_cddl().unwrap());
+  for file in fs::read_dir("tests/cddl/").unwrap() {
+    let file = file.unwrap();
+    let cddl_contents = fs::read_to_string(file.path()).unwrap();
+    let mut l = Lexer::new(&cddl_contents);
+    let mut p = Parser::new(&mut l).unwrap();
+    println!("{:#?}", p.parse_cddl().unwrap());
+  }
 }
