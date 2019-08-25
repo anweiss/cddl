12e1080476f2992c12909e5710a3a956211b2ed1
Andrew Weiss <andrew.weiss@outlook.com>
2019-08-25 21:36:47 +0000

    CI website build

--- 8.bootstrap.js
+++ 8.bootstrap.js
@@ -1,15 +1,1 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[8],{
-
-/***/ "./editor.js":
-/*!*******************!*\
-  !*** ./editor.js ***!
-  \*******************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var monaco_editor__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! monaco-editor */ \"./node_modules/monaco-editor/esm/vs/editor/editor.main.js\");\n\n\n// self.MonacoEnvironment = {\n// \tgetWorkerUrl: function (moduleId, label) {\n// \t\tif (label === 'json') {\n// \t\t\treturn './json.worker.bundle.js';\n// \t\t}\n// \t\tif (label === 'css') {\n// \t\t\treturn './css.worker.bundle.js';\n// \t\t}\n// \t\tif (label === 'html') {\n// \t\t\treturn './html.worker.bundle.js';\n// \t\t}\n// \t\tif (label === 'typescript' || label === 'javascript') {\n// \t\t\treturn './ts.worker.bundle.js';\n// \t\t}\n// \t\treturn './editor.worker.bundle.js';\n// \t}\n// }\n\nwindow.editor = monaco_editor__WEBPACK_IMPORTED_MODULE_0__[\"editor\"].create(document.getElementById('cddlDataDefinition'), {\n  minimap: {\n    enabled: false\n  }\n});\n\n//# sourceURL=webpack:///./editor.js?");
-
-/***/ })
-
-}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{151:function(e,s,o){"use strict";o.r(s),o.d(s,"conf",function(){return n}),o.d(s,"language",function(){return t});var n={comments:{lineComment:"REM"},brackets:[["{","}"],["[","]"],["(",")"]],autoClosingPairs:[{open:"{",close:"}"},{open:"[",close:"]"},{open:"(",close:")"},{open:'"',close:'"'}],surroundingPairs:[{open:"[",close:"]"},{open:"(",close:")"},{open:'"',close:'"'}],folding:{markers:{start:new RegExp("^\\s*(::\\s*|REM\\s+)#region"),end:new RegExp("^\\s*(::\\s*|REM\\s+)#endregion")}}},t={defaultToken:"",ignoreCase:!0,tokenPostfix:".bat",brackets:[{token:"delimiter.bracket",open:"{",close:"}"},{token:"delimiter.parenthesis",open:"(",close:")"},{token:"delimiter.square",open:"[",close:"]"}],keywords:/call|defined|echo|errorlevel|exist|for|goto|if|pause|set|shift|start|title|not|pushd|popd/,symbols:/[=><!~?&|+\-*\/\^;\.,]+/,escapes:/\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,tokenizer:{root:[[/^(\s*)(rem(?:\s.*|))$/,["","comment"]],[/(\@?)(@keywords)(?!\w)/,[{token:"keyword"},{token:"keyword.$2"}]],[/[ \t\r\n]+/,""],[/setlocal(?!\w)/,"keyword.tag-setlocal"],[/endlocal(?!\w)/,"keyword.tag-setlocal"],[/[a-zA-Z_]\w*/,""],[/:\w*/,"metatag"],[/%[^%]+%/,"variable"],[/%%[\w]+(?!\w)/,"variable"],[/[{}()\[\]]/,"@brackets"],[/@symbols/,"delimiter"],[/\d*\.\d+([eE][\-+]?\d+)?/,"number.float"],[/0[xX][0-9a-fA-F_]*[0-9a-fA-F]/,"number.hex"],[/\d+/,"number"],[/[;,.]/,"delimiter"],[/"/,"string",'@string."'],[/'/,"string","@string.'"]],string:[[/[^\\"'%]+/,{cases:{"@eos":{token:"string",next:"@popall"},"@default":"string"}}],[/@escapes/,"string.escape"],[/\\./,"string.escape.invalid"],[/%[\w ]+%/,"variable"],[/%%[\w]+(?!\w)/,"variable"],[/["']/,{cases:{"$#==$S2":{token:"string",next:"@pop"},"@default":"string"}}],[/$/,"string","@popall"]]}}}}]);
