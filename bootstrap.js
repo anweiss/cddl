9f5021ae62778d21e144585622e7f40ff01fa2dc
Andrew Weiss <anweiss@github.com>
2019-08-25 12:16:36 -0400

    update build

--- bootstrap.js
+++ bootstrap.js
@@ -162,7 +162,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/cddl_bg.wasm":"7c8d49d776deffc48fa8"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/cddl_bg.wasm":"dfd43f37cdb56b8629e6"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
