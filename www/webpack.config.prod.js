a52bb182f022bff711b6fcd1a22a7ae36fdfaab0
anweiss <2326106+anweiss@users.noreply.github.com>
2022-03-23 14:03:52 -0400

    update web assets and bundling

--- www/webpack.config.prod.js
+++ www/webpack.config.prod.js
@@ -1,14 +1,18 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: './bootstrap.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bootstrap.js',
+    filename: 'app.js',
   },
   mode: 'production',
-  plugins: [new CopyWebpackPlugin({ patterns: ['index.html'] })],
+  plugins: [
+    new CopyWebpackPlugin({ patterns: ['index.html'] }),
+    new MonacoWebpackPlugin(),
+  ],
   module: {
     rules: [
       {
