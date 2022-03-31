3aede16f08b38524d9b5165ad1675c1cf733dbe2
anweiss <2326106+anweiss@users.noreply.github.com>
2022-03-31 12:25:40 -0400

    remove repl

--- src/lib.rs
+++ src/lib.rs
@@ -545,9 +545,6 @@ pub mod error;
 pub mod lexer;
 /// Parser for CDDL
 pub mod parser;
-/// Basic REPL for CDDL lexing
-#[cfg(not(target_arch = "wasm32"))]
-pub mod repl;
 /// CDDL tokens for lexing
 pub mod token;
 /// Validators for JSON and CBOR data structures
